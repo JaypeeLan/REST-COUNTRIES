c7e52eee2e5404cb2bc3f2edc1339207f830d660
JohnPaul Laniran <jplaniran01@gmail.com>
2022-09-18 17:00:31 +0100

    added animation

--- src/component/DisplayCountries.jsx
+++ src/component/DisplayCountries.jsx
@@ -1,16 +1,19 @@
 import React from 'react'
+import Fade from 'react-reveal/Fade'
 
 const DisplayCountries = ({ name, flags, cap, population, region }) => {
   return (
-    <div className="country">
-      <img src={flags.png} alt="flag" />
-      <div className="country-info"> 
-        <h3>{name.common}</h3>
-        <p>Population: {population}</p>
-        <p>Capital: {cap}</p>
-        <p>Region: {region}</p>
+    <Fade>
+      <div className="country">
+        <img src={flags.png} alt="flag" />
+        <div className="country-info">
+          <h3>{name.common}</h3>
+          <p>Population: {population}</p>
+          <p>Capital: {cap}</p>
+          <p>Region: {region}</p>
+        </div>
       </div>
-    </div>
+    </Fade>
   )
 }
 
